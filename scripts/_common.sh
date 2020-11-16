8e6fd06c5fa6e23cf53ff9ee7fd72221ec40ea4b
ericgaspar <junk.eg@free.fr>
2020-11-16 09:09:47 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,3 +18,17 @@ pkg_dependencies="nim libsass-dev redis-server"
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+
+# Execute a command as another user
+# usage: ynh_exec_as USER COMMAND [ARG ...]
+ynh_exec_as() {
+  local USER=$1
+  shift 1
+
+  if [[ $USER = $(whoami) ]]; then
+    eval "$@"
+  else
+    sudo -u "$USER" "$@"
+  fi
+}
